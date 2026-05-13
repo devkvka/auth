3229a50222157165e4ce89ece2d0547a2a39f4f1
Diego Koivukangas <devkvka@gmail.com>
2026-05-13 15:20:55 +0300

    setup project structure

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"net/http"
+	"log"
+
+	"auth/internal/handler"
 )
 
 func main() {
+	fmt.Println(" !-- Booting auth service ... ")
 
+	port := flag.String("-port", ":8080", "port to serve on")
+	flag.Parse()
+
+	fmt.Println(" !-- Setting up handlers ... ")
+	http.HandleFunc("/verify", handler.Verify)
+	http.HandleFunc("/login", handler.Login)
+	http.HandleFunc("/create-account", handler.CreateAccount)
+	
+	fmt.Printf(" !-- Setup ready, serving on: localhost%s \n", *port)
+	log.Fatal(http.ListenAndServe(*port, nil))
 }
---
+++ internal/handler/handler.go
@@ -0,0 +1,8 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+)
+
+
